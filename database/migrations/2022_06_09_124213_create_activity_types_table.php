e3aefba984dda928c79187c3f71c86a354eb5508
Chris Groves <cdgroves1@gmail.com>
2022-06-09 15:15:36 +0100

    update

--- database/migrations/2022_06_09_124213_create_activity_types_table.php
+++ database/migrations/2022_06_09_124213_create_activity_types_table.php
@@ -15,6 +15,13 @@ class CreateActivityTypesTable extends Migration
     {
         Schema::create('activity_types', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name',250);
+            $table->string('category',100);
+            $table->string('grouping',100);
+            $table->string('name_short',100);
+            $table->unsignedInteger('list_position');
+            $table->unsignedBigInteger('updated_by');
             $table->timestamps();
         });
     }
